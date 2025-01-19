fb1bd28c9e103f05035ef87c3eaaf4f5ef7625f5
SakaiTaka23 <tt3.sakai@gmail.com>
2025-01-19 14:39:27 +0900

    made register and login button different

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,46 +1,59 @@
-"use client"
+'use client';
 
-import "./globals.css";
-import React, {useEffect, useState} from "react";
-import {redirect} from "next/navigation";
+import './globals.css';
+import React, { useEffect, useState } from 'react';
+import { redirect } from 'next/navigation';
 import Cookies from 'js-cookie';
 import jwt from 'jsonwebtoken';
 
-const KEYCLOAK_URL = 'http://localhost:8080/realms/myrealm/protocol/openid-connect/auth'
+const KEYCLOAK_BASE_URL =
+  'http://localhost:8080/realms/myrealm/protocol/openid-connect';
+const KEYCLOAK_SIGN_IN_URL = `${KEYCLOAK_BASE_URL}/auth`;
+const KEYCLOAK_SIGN_UP_URL = `${KEYCLOAK_BASE_URL}/registrations`;
 const params = new URLSearchParams({
-    client_id: 'frontend-client',
-    redirect_uri: 'http://localhost:3000/api/callback',
-    response_type: 'code',
-    scope: 'openid profile email',
+  client_id: 'frontend-client',
+  redirect_uri: 'http://localhost:3000/api/callback',
+  response_type: 'code',
+  scope: 'openid profile email',
 });
 
 type DecodedToken = {
-    preferred_username?: string
-}
+  preferred_username?: string;
+};
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
-    const [nickname, setNickname] = useState<string | undefined>(undefined)
-    useEffect(() => {
-        const token = Cookies.get('id_token')
-        if (token) {
-            const decoded = jwt.decode(token) as DecodedToken
-            setNickname(decoded?.preferred_username)
-        }
-    }, [])
-
-    const loginUrl = `${KEYCLOAK_URL}?${params.toString()}`
-    const handleLogin = () => {
-        redirect(loginUrl);
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const [nickname, setNickname] = useState<string | null>(null);
+  useEffect(() => {
+    const token = Cookies.get('id_token');
+    if (token) {
+      const decoded = jwt.decode(token) as DecodedToken;
+      setNickname(decoded?.preferred_username || null);
     }
+  }, []);
 
-    return (
-        <html lang="en">
-        <body>
-        <button onClick={handleLogin}>login</button>
-        <button>register</button>
-        <h1>{nickname}</h1>
+  const handleLogin = () => {
+    redirect(`${KEYCLOAK_SIGN_IN_URL}?${params.toString()}`);
+  };
+
+  const handleSignUp = () => {
+    redirect(`${KEYCLOAK_SIGN_UP_URL}?${params.toString()}`);
+  };
+
+  return (
+    <html lang='en'>
+      <body>
+        {nickname ? (
+          <h1>Hello {nickname}</h1>
+        ) : (
+          <>
+            <button onClick={handleLogin}>login</button>
+            <button onClick={handleSignUp}>register</button>
+          </>
+        )}
         {children}
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
