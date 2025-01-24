b6fb3d60da5bdbe28f50a39d35a61033401297cc
SakaiTaka23 <tt3.sakai@gmail.com>
2025-01-24 22:36:49 +0900

    removed code from original app

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,37 +1,7 @@
-import { signIn, signOut } from '@/app/auth';
-
 export default function Home() {
   return (
-    <>
+    <div>
       <h1>Home</h1>
-      <SignIn />
-      <SignOut />
-    </>
-  );
-}
-
-function SignIn() {
-  return (
-    <form
-      action={async () => {
-        'use server';
-        await signIn('keycloak');
-      }}
-    >
-      <button type='submit'>Signin with Keycloak</button>
-    </form>
-  );
-}
-
-function SignOut() {
-  return (
-    <form
-      action={async () => {
-        'use server';
-        await signOut();
-      }}
-    >
-      <button type='submit'>Sign Out</button>
-    </form>
+    </div>
   );
 }
--- client/app/private/page.tsx
+++ client/app/private/page.tsx
@@ -1,13 +1,7 @@
-import { auth } from '@/app/auth';
-
 export default async function Private() {
-  const session = await auth();
-  if (!session) return <div>Not authenticated</div>;
-
   return (
     <div>
       <h1>This is private page</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
 }
